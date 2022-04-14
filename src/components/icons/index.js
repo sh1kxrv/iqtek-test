c50355412a0b978df958857db09f3defd9689b3f
Егор <ItsAwware@yandex.ru>
2022-04-14 19:21:05 +0600

    chore: icons update

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,4 @@
 export { default as IconPerson } from './person.vue'
 export { default as IconSearch } from './search.vue'
 export { default as IconTrash } from './trash.vue'
+export { default as IconEdit } from './edit.vue'
